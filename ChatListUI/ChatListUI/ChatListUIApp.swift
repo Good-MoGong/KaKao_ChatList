3158e6e4bfde262323b60ebec95df923cb342d27
Jiyoung Ahn <wl950204@naver.com>
2023-06-27 22:35:56 +0900

    chore : kakao chat SwiftUI 구현 파일 커밋

---
+++ ChatListUI/ChatListUI/ChatListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatListUIApp.swift
+//  ChatListUI
+//
+//  Created by 안지영 on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
